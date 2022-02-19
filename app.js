4e465a7660f8a8eb20daafdf45ee11140415147e
Kenny <kenny.wj.li@gmail.com>
2022-02-19 17:30:54 -0500

    connected app to mongoDB

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ function fetchApi(word){
     let url = 'https://api.datamuse.com/words?sp=?????'
     fetch(url).then(res => res.json()).then(result => data(result, word))
 }
+
+
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
