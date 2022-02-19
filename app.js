4ca275e7820fd192743c3bca6f8d00e61732327c
Kenny <kenny.wj.li@gmail.com>
2022-02-19 17:50:25 -0500

    added user model

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-function data(result, word){
-    console.log(result)
-}
-
-function fetchApi(word){
-    let url = 'https://api.datamuse.com/words?sp=?????'
-    fetch(url).then(res => res.json()).then(result => data(result, word))
-}
-
-
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+    res.send("Hello World")
+});
+
+app.use("/api/users", users);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => res.send("Hello World"));
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
+// function data(result, word){
+//     console.log(result)
+// }
+
+// function fetchApi(word){
+//     let url = 'https://api.datamuse.com/words?sp=?????'
+//     fetch(url).then(res => res.json()).then(result => data(result, word))
+// }
