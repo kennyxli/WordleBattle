9212b615626bd4c20d5f1634a63735faef10bac5
Kenny <kenny.wj.li@gmail.com>
2022-02-19 20:55:03 -0500

    set up a route for user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,6 @@ const User = require("../../models/User");
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
-
     User.findOne({ username: req.body.username })
       .then(user => {
         if (user) {
@@ -28,7 +27,29 @@ router.post('/register', (req, res) => {
           })
         }
       })
-  })
+})
+
+router.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({username})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({username: 'This user does not exist'})
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        res.json({msg: 'Success'})
+                    }else{
+                        return res.status(400).json({password: 'Incorrect password'})
+                    }
+                    
+                })
+        })
+})
 
 
 module.exports = router;
